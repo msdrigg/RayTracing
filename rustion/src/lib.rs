ec99389fa5b140c2482ccc4be08c5f6cde4f066d
msdrigg <m.scott.driggers@gmail.com>
2021-02-19 17:42:04 -0500

    Fixed first rust functions

--- rustion/src/lib.rs
+++ rustion/src/lib.rs
@@ -4,6 +4,7 @@ mod equations;
 use std::array;
 
 use pyo3::prelude::*;
+use numpy::{IntoPyArray, PyArrayDyn, PyReadonlyArrayDyn};
 use pyo3::wrap_pyfunction;
 use ndarray::prelude::{Array1};
 
@@ -15,7 +16,19 @@ fn solve_yp_vector(ptr: *mut f64, len: usize) -> *mut f64 {
 }
 
 #[pymodule]
-fn rustion(py: Python, m: &PyModule) -> PyResult<()> {
+fn rustion(py: Python<'_>, m: &PyModule) -> PyResult<()> {
+    // wrapper of `axpy`
+    #[pyfn(m, "axpy")]
+    fn axpy_py<'py>(
+        py: Python<'py>,
+        a: f64,
+        x: PyReadonlyArrayDyn<f64>,
+        y: PyReadonlyArrayDyn<f64>,
+    ) -> &'py PyArrayDyn<f64> {
+        let x = x.as_array();
+        let y = y.as_array();
+        axpy(a, x, y).into_pyarray(py)
+    }
     m.add_function(wrap_pyfunction!(solve_yp_vector, m)?)?;
 
     Ok(())
